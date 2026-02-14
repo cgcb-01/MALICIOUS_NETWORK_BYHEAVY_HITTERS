2b67440a913d5070ad39842be4f40e0ff3538622
cgcb-01 <206439682+cgcb-01@users.noreply.github.com>
2026-02-14 09:46:24 +0530

    Updated

--- data/dataGeneration/data_gen.py
+++ data/dataGeneration/data_gen.py
@@ -1,3 +1,4 @@
 from pyspark.sql import SparkSession
 def generate_data(freq_threshold,call_status_count,timestamp_out_count):
     spark=SparkSession.builder.appName("DataGeneration").getOrCreate()
+    
